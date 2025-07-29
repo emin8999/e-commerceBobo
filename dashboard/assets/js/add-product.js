063063f841e1dcdd30b7a966926950c2123e92c7
unknown <rustam.kerimli.wf@gmail.com>
2025-07-29 15:47:08 +0400

    enhance product addition form with dynamic size input and improved layout

--- dashboard/assets/js/add-product.js
+++ dashboard/assets/js/add-product.js
@@ -11,6 +11,51 @@ document.addEventListener("DOMContentLoaded", () => {
     option.textContent = store.name;
     storeSelect.appendChild(option);
   });
+  const addBtn = document.querySelector(".add-size-btn");
+  const sizeWrapper = document.querySelector(".size-wrapper");
+
+  addBtn.addEventListener("click", () => {
+    const newSizeBlock = document.createElement("div");
+    newSizeBlock.classList.add("size-items");
+
+    newSizeBlock.innerHTML = `
+      <select id="productSizes" class="size-input">
+                <option value="">Size</option>
+                <option value="2XS">2XS</option>
+                <option value="XS">XS</option>
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+                <option value="XL">XL</option>
+                <option value="2XL">2XL</option>
+              </select>
+              <input
+                type="number"
+                id="productQuantity"
+                placeholder="Quantity"
+                class="quantity-input"
+                required
+              />
+    `;
+
+    sizeWrapper.insertBefore(newSizeBlock, addBtn);
+  });
+  const storeData = {
+    name: "BOBO Store",
+    id: "store123",
+    address: "Baku, Azerbaijan",
+    owner: "Rustam Kerimli",
+  };
+
+  localStorage.setItem("storeInfo", JSON.stringify(storeData));
+
+  const storeInput = document.getElementById("productStore");
+  const stored = localStorage.getItem("storeInfo");
+
+  if (stored && storeInput) {
+    const storeObj = JSON.parse(stored);
+    storeInput.value = storeObj.name;
+  }
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
