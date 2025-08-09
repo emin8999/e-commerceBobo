8e9d972cf67d8bc77e354f438d50935f0eae1a53
YunisPashayev <yunispasayev10@gmail.com>
2025-08-09 23:05:02 +0400

    Import changed

--- e-commerce-backend/src/main/java/com/e_commerce_backend/repository/OrderRepository.java
+++ e-commerce-backend/src/main/java/com/e_commerce_backend/repository/OrderRepository.java
@@ -5,12 +5,13 @@ import com.e_commerce_backend.entity.UserEntity;
 import com.e_commerce_backend.enums.OrderStatus;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
