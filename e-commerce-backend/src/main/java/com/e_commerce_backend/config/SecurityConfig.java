4b22e1d7c2212d2e5f7e4f0d6c8bc5b2aecf2438
YunisPashayev <yunispasayev10@gmail.com>
2025-08-17 21:26:41 +0400

    cors * filtered

--- e-commerce-backend/src/main/java/com/e_commerce_backend/config/SecurityConfig.java
+++ e-commerce-backend/src/main/java/com/e_commerce_backend/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of("*")); // Frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
