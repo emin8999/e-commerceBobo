85ba68991ef25b7c33150d26a4818486213fba18
YunisPashayev <yunispasayev10@gmail.com>
2025-08-21 17:37:40 +0400

    change * security

--- e-commerce-backend/src/main/java/com/e_commerce_backend/controller/StoreController.java
+++ e-commerce-backend/src/main/java/com/e_commerce_backend/controller/StoreController.java
@@ -39,6 +39,7 @@ public class StoreController {
     }
 
     @PostMapping("/login")
+    @PreAuthorize("hasRole('STORE')")
     public ResponseEntity<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
         LoginResponseDto response = storeService.login(loginRequestDto);
         return ResponseEntity.ok(response);
